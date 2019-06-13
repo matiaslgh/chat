75e638f8a68698b08fff4da9a7c7e48f19ecd626
Matias Luraghi <matiaslgh@gmail.com>
2019-06-13 14:46:14 -0300

    Document method

--- frontend/utils/isomorphic.js
+++ frontend/utils/isomorphic.js
@@ -1,5 +1,10 @@
 import Router from 'next/router';
 
+/**
+ * Isomorphic function to redirect either in the server or the browser
+ * @param {string} pathname relative url
+ * @param {Object} context which has req and res if is the server
+ */
 export const redirect = (pathname, { req, res }) => {
   const isServer = !!req;
 
