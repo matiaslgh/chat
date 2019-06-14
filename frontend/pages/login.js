589c34d3a85b2bc6d5219c03bc6756a240b095dc
Matias Luraghi <matiaslgh@gmail.com>
2019-06-14 10:41:54 -0300

    If a logged in user enters to /login must be redirected to /

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import LoginContainer from '../containers/LoginContainer';
 import Layout from '../components/Layout';
+import { getCurrentUser } from '../utils/auth';
+import { redirect } from '../utils/isomorphic';
 
 const Login = () => (
   <Layout>
     <LoginContainer />
   </Layout>
 );
+
+Login.getInitialProps = ctx => {
+  const currentUser = getCurrentUser(ctx);
+  if (currentUser) {
+    redirect('/', ctx);
+  }
+};
 
 export default Login;
